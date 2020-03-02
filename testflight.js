b5dd32a47c53ad918c1dfe88ca010a9d9acdc9f4
sy <623984375@qq.com>
2020-03-02 13:06:15 +0800

    饿了么加入翻牌

--- testflight.js
+++ testflight.js
@@ -137,7 +137,7 @@ const url = "https://testflight.apple.com/join/";
  * 填入要监测的appkey。从testfligt地址获取。
  * 例如"VCIvwk2g,wArXdacJ,2vnRvOTX,LzjySbQx,IdFRwmNy,qDkBu2ur,4Qt2lIm5,ZzqOu8tX,ftCqFe6D,fy7LvHVA,QKqitFwc"
 */
-const appkey = "VCIvwk2g";
+const appkey = "VCIvwk2g,LzjySbQx";
 
 //是否在没有tf位置的时候任然弹出通知，默认不弹出,防止过多无用通知。
 var isNOtify = false;
