281e282d0bc49c47ff67d96ce6c91a33a41f17b1
sy <623984375@qq.com>
2020-03-03 13:00:16 +0800

    1

--- testflight.js
+++ testflight.js
@@ -139,7 +139,7 @@ const url = "https://testflight.apple.com/join/";
 */
 const appkey = "VCIvwk2g,LzjySbQx";
 
-//是否在没有tf位置的时候任然弹出通知，默认不弹出,防止过多无用通知。
+//是否在没有tf位置的时候仍然弹出通知，默认不弹出,防止过多无用通知。
 var isNOtify = false;
 const fullstr = /(此 Beta 版本的测试员已满)|(此 Beta 版本目前不接受任何新测试员)/;
 const appnamereg = /<title>加入 Beta 版“(.+)” - TestFlight - Apple<\/title>/;
