ba02dee488f24fe3d1155a146262320aa989090b
songyang <623984375@qq.com>
2020-11-15 17:42:38 +0800

    修复tf

--- testflight.js
+++ testflight.js
@@ -1,10 +1,23 @@
+/** 
+ * 注意: 
+ * 在这里你可以使用完整的 EnvJs 环境
+ * 
+ * 同时: 
+ * 你`必须`手动调用 $done()
+ * 
+ * 因为: 
+ * BoxJs 不为主动执行的脚本调用 $done()
+ * 而把 $done 的时机完全交由脚本控制
+ * 
+ * 最后: 
+ * 这段脚本是可以直接运行的!
+ */
 
 /**
  * 参考chavyleung和NobyDa的写法
  * 写入要监测的公测tf appkey，当有空位的时候会弹出通知。
  * 建议task时间间隔小点。
  */
-const sy = init()
 const title = 'testfilght';
 const url = "https://testflight.apple.com/join/";
 
@@ -13,139 +26,114 @@ const url = "https://testflight.apple.com/join/";
  * 例如"VCIvwk2g,wArXdacJ,2vnRvOTX,LzjySbQx,IdFRwmNy,qDkBu2ur,4Qt2lIm5,ZzqOu8tX,ftCqFe6D,fy7LvHVA,QKqitFwc"
 */
 //const appkey = "VCIvwk2g,1SyedSId";
-const appkey=getdata('appkey');
+const appkey = $.getdata('appkey');
 
 //是否在没有tf位置的时候仍然弹出通知，默认不弹出,防止过多无用通知。
-var isNOtify = getdata('testflight_isnotify');
-const fullstr = /(此 Beta 版本的测试员已满)|(此 Beta 版本目前不接受任何新测试员)｜This beta is full/;
-const appnamereg = /<title>加入 Beta 版“(.+)” - TestFlight - Apple<\/title>/;
+var isNOtify = $.getdata('testflight_isnotify');
+const fullstr = /版本的测试员已满|版本目前不接受任何新测试员|This beta is full|This beta isn't accepting any new testers right now/;
+const appnamereg = /Join the (.+) beta - TestFlight - Apple/;
+const appnameregch = /加入 Beta 版“(.+)” - TestFlight - Apple/;
 var proarray = new Array();
 getResult();
 
 function getResult() {
-    var upstr = '已有空位，抓紧上车';
-    var apps = new Array(); //定义一数组
-    apps = appkey.split(","); //字符分割
-    var resultstr = false;
-    var logdata={};
-    for (var i = 0; i < apps.length; i++) {
+  var upstr = '已有空位，抓紧上车';
+  var apps = new Array(); //定义一数组
+  apps = appkey.split(","); //字符分割
+  var resultstr = false;
+  var logdata = {};
+  for (var i = 0; i < apps.length; i++) {
 
-        var p = new Promise(function (resolve) {
-            var lol = {
-                url: url + apps[i],
-                headers: {
-                    'User-Agent': '[{"key":"User-Agent","value":" Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2357.130 Safari/537.36 qblink wegame.exe QBCore/3.70.66.400 QQBrowser/9.0.2524.400","type":"text","enabled":true,"description":""},{"key":"X-Requested-With","value":" XMLHttpRequest","type":"text","enabled":false,"description":""}]',
-                },
-            };
-            sy.get(lol, function (error, response, data) {
-                try {
-                    appnamereg.test(data);
-                    var appname = appnamereg.exec(data);
-                    if (!appname != null) {
-                        var reg = /“.+”/
-                        var item = reg.exec(appname[0]);
-                        var name = item[0].replace('“', '').replace('”', '');
-                        if (!fullstr.test(data)) {
-                            logdata[name]={
-                                'has':true,
-                                'context':upstr + '👉:' + lol.url + '\n'
-                            }
-                            resultstr=true;
-                        }
-                        else{
-                            logdata[name]={
-                                'has':false,
-                                'context':':暂无车位'+'\n'
-                            }
-                        }
-                    }
-                    resolve('done');
-                }
-                catch (errr) {
-                    resolve('done');
-                }
-
-            });
-        });
+       var lol = {
+        url: url + apps[i],
+        headers: {
+          'User-Agent': '[{"key":"User-Agent","value":" Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2357.130 Safari/537.36 qblink wegame.exe QBCore/3.70.66.400 QQBrowser/9.0.2524.400","type":"text","enabled":true,"description":""},{"key":"X-Requested-With","value":" XMLHttpRequest","type":"text","enabled":false,"description":""}]',
+        },
+      };
 
 
-        proarray[i] = p;
+    var p = new Promise(function (resolve) {
+        $.get(lol, (error, response, data) => {
+       
+      try {
+          var dataStr=JSON.stringify(data);
+         var appname;
+             if(appnamereg.test(dataStr)){
+             
+           appname = dataStr.match(appnamereg);
+         }
+         else if(appnameregch.test(dataStr)) {
+             
+          appname =dataStr.match(appnameregch);
+         }
+         else{
+           resolve()
+          
+            return;
+         }
+    
+
+           var name = appname[1];
+            $.log(name)
+            if (!fullstr.test(dataStr)) {
+              logdata[name] = {
+                'has': true,
+                'context': upstr + '👉:' + lol.url + '\n'
+              }
+              resultstr = true;
+            }
+             else {
+              logdata[name] = {
+                'has': false,
+                'context': ':暂无车位' + '\n'
+              }
+               resolve();
+          } 
+           resolve();
+         
+        }
+        catch (errr) {
+          resolve();
+        }
+        })
+
+
+     
+  });
+     proarray[i] = p;
+  }
+  Promise.all(proarray).then((result) => {
+    var hastr = '';
+    var nostr = '';
+    $.log(JSON.stringify(logdata));
+    for (var name in logdata) {
+      if (logdata[name].has) {
+        hastr = hastr + '[' + name + ']' +'\n'+ logdata[name].context;
+      }
+      else {
+        nostr = nostr + '[' + name + ']' + '\n'+ logdata[name].context;
+      }
     }
-    Promise.all(proarray).then((result) => {
-        var hastr='';
-        var nostr='';
-        for(var name in logdata){
-            if(logdata[name].has){
-                hastr=hastr+'[' + name + ']'+logdata[name].context;
-            }
-            else{
-                nostr=nostr+'[' + name + ']'+logdata[name].context;
-            }
-        }
-        if (resultstr) {
-            sy.msg('', '', hastr+nostr);
-        }
-        else{
-            if(isNOtify=="true"){
-                sy.msg('', '', hastr+nostr);
-            }
-        }
-        sy.log(hastr+nostr);
-         sy.done();
-    }).catch((error) => {
-        sy.log(error)
-    });
+
+    if (resultstr) {
+      
+      $.msg(hastr + nostr);
+    }
+    else {
+      if (isNOtify == "true") {
+        $.msg(hastr + nostr);
+      }
+    }
+    $.done()
+  }).catch((error) => {
+    $.log(error)
+    $.done()
+  });
 
 
 }
 
+// $done() 或 $.done() 都可以
 
-function init() {
-    isSurge = () => {
-      return undefined === this.$httpClient ? false : true
-    }
-    isQuanX = () => {
-      return undefined === this.$task ? false : true
-    }
-    getdata = (key) => {
-      if (isSurge()) return $persistentStore.read(key)
-      if (isQuanX()) return $prefs.valueForKey(key)
-    }
-    setdata = (key, val) => {
-      if (isSurge()) return $persistentStore.write(key, val)
-      if (isQuanX()) return $prefs.setValueForKey(key, val)
-    }
-    msg = (title, subtitle, body) => {
-      if (isSurge()) $notification.post(title, subtitle, body)
-      if (isQuanX()) $notify(title, subtitle, body)
-    }
-    log = (message) => console.log(message)
-    get = (url, cb) => {
-      if (isSurge()) {
-        $httpClient.get(url, cb)
-      }
-      if (isQuanX()) {
-        url.method = 'GET'
-        $task.fetch(url).then((resp) => cb(null, {}, resp.body))
-      }
-    }
-    post = (options, callback) => {
-      if (isQuanX()) {
-        if (typeof options == "string") options = { url: options }
-        options["method"] = "POST"
-        $task.fetch(options).then(response => {
-          response["status"] = response.statusCode
-          callback(null, response, response.body)
-        }, reason => callback(reason.error, null, null))
-      }
-      if (isSurge()) $httpClient.post(options, callback)
-    }
-    done = (value = {}) => {
-      $done(value)
-    }
-    return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
-  }
-  
-    
-        
-  
+
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
